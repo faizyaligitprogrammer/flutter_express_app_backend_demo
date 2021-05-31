cec7519bf3e20243f9cc0d0620d8f32f88720f9b
Manuel Robert <manuelrobz@outlook.com>
2021-05-31 17:03:19 +0530

    basic login register completed

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/userSchema');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/register', (req, res) => {
+  let newUser = User({
+    'username': req.body.username,
+    'password': req.body.password
+  });
+  newUser.save((err, usr) => {
+    if (err) {
+      return res.status(500).json({ msg: err });
+    } else {
+      return res.status(200).json({ msg: "User saved successfully", usr: usr.username });
+    }
+  });
+});
+
+router.post('/login', (req, res) => {
+  User.findOne({ username: req.body.username, password: req.body.password }, (err, usr) => {
+    if (err) {
+      return res.status(500).json({ msg: err });
+    } else {
+      return res.status(200).json({ msg: "Login successfull", usr: usr.username });
+    }
+  });
 });
 
 module.exports = router;
